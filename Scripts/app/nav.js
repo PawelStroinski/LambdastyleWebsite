c030c043317862e423bb573c31bb2aa43c1d2ee3
Paweł Stroiński <pol84@live.com>
2014-07-01 01:42:42 +0100

    Add empty download tabs and make navigation working

---
+++ Scripts/app/nav.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    $('.navbar a').on('click', function (event) {
+        var href = $(event.target).attr('href');
+        scrollTop(href === '#' ? 0 : $(href).offset().top);
+        event.preventDefault();
+    });
+
+    $('.download a').on('click', function (event) {
+        var href = $(event.target).attr('href');
+        scrollTop($('#download').offset().top);
+        $('.nav-tabs a[href=' + href + ']').click();
+        event.preventDefault();
+    });
+
+    function scrollTop(value) {
+        $('html, body').animate({ scrollTop: value }, 100);
+    }
+}());
