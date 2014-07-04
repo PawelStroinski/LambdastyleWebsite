0dd5157adb1361edea7701fb25625135134852bc
Paweł Stroiński <pol84@live.com>
2014-07-04 01:02:59 +0100

    Add bundles

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace LambdastyleWebsite
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/bundle").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/bootswatch.min.css",
+                "~/Content/app.css"));
+            bundles.Add(new ScriptBundle("~/Scripts/bundle-of-bundles").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery.signalR-{version}.js",
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/app/bundle.js",
+                "~/Scripts/app/nav.js"));
+        }
+    }
+}
